ba0587a41ea2e95b9cddb37960742ae6e5b388a8
Terry <terrylin1990k@gmail.com>
2017-03-02 12:24:09 -0500

    Breaking the page into multiple components and display them
    Breaks up the page into multiple components and using the export and
    import method to display them on index.js

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,24 @@ import React from "react";
 //Importing render method for rendering purpose
 import {render} from "react-dom";
 
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
+
 class App extends React.Component{
 	render(){
 		return(
-			<div>
-				<h1>Hello</h1>
+			// Use ClassName instead of class to use Boostrap since it's not HTML
+			<div className="container">
+				<div className = "row">
+					<div>
+						<div className = "col-xs-10 col-xs-offset-1">
+							<Header/>
+						</div>
+						<div className = "col-xs-10 col-xs-offset-1">
+							<Home/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
